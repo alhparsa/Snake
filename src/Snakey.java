4821a50b3386861da0e92e5978e6e13c21582f5b
hyoon1998 <hansooyoon@hotmail.com>
2017-06-03 17:49:29 +0800

    name change

--- src/Snakey.java
+++ src/Snakey.java
@@ -2,14 +2,14 @@ import javax.swing.JFrame; //imports JFrame library
 import javax.swing.JButton; //imports JButton library
 import java.awt.GridLayout; //imports GridLayout library
 
-public class main {
+public class Snakey {
 
     JFrame frame=new JFrame(); //creates frame
     JButton[][] grid; //names the grid of buttons
 
-    public main(int width, int length){ //constructor
+    public Snakey(int width, int length){ //constructor
         frame.setLayout(new GridLayout(width,length)); //set layout
-        grid=new JButton[width][length]; //allocate the size of grid
+        grid=new JButton[width][length]; //allocate the size of gri
         for(int y=0; y<length; y++){
             for(int x=0; x<width; x++){
                 grid[x][y]=new JButton(); //creates new button
@@ -23,6 +23,7 @@ public class main {
         frame.setSize(500, 500);
     }
     public static void main(String[] args) {
-        new main(20,20);//makes new ButtonGrid with 2 parameters
+        
+        Snakey snake =new Snakey(20,20);//makes new ButtonGrid with 2 parameters
     }
 }
