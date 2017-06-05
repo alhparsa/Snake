26006620145b8e18c632ed0ebf4856806e0d895a
Parsa <alh.parsa@gmail.com>
2017-06-05 23:04:31 +0800

    keyListener has been added

--- src/Snakey.java
+++ src/Snakey.java
@@ -2,17 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Snakey {
 
-    static JFrame frame=new JFrame(); //creates frame
-    static JButton[][] grid; //names the grid of buttons
+public class Snakey implements KeyListener{
+    snake Snake = new snake();
+    JFrame frame=new JFrame(); //creates frame
+    JButton[][] grid; //names the grid of buttons
 
-    public static void main(String[] args) {
-        int width=20;
-        int length=20;
+    private Snakey(int width, int length){ //constructor
         frame.setLayout(new GridLayout(width,length)); //set layout
-        grid=new JButton[width][length]; //allocate the size of gri
+        grid=new JButton[width][length]; //allocate the size of grid
         for(int y=0; y<length; y++){
             for(int x=0; x<width; x++){
                 grid[x][y]=new JButton(); //creates new button
@@ -23,21 +24,33 @@ public class Snakey {
 
         snake food = new snake();
 
-        Timer timer = new Timer(1000, new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                food.food(grid, width,length);
-            }
-        });
+        Timer timer = new Timer(1000, e -> food.food(grid, width,length));
         timer.setRepeats(true);
         timer.start();
-
+        frame.setFocusable(true);
+        frame.addKeyListener(this);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack(); //sets appropriate size for frame
         frame.setVisible(true); //makes frame visible
         frame.setSize(500, 500);
+    }
+    public static void main(String[] args) {
+        new Snakey(20,20);//makes new ButtonGrid with 2 parameters
+    }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //nothing
+    }
 
-        //grid is created
+    @Override
+    public void keyPressed(KeyEvent e) {
+        Snake.keyPress(e.getKeyCode());
+        System.out.println("transferred");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
--- src/snake.java
+++ src/snake.java
@@ -5,22 +5,26 @@ import java.awt.*;
  * Created by Parsa on 2017-05-18.
  */
 
-public class snake {
+class snake {
     int size = 2;
+    int keyID;
+
 
 
     private int movement() {
         return size;
     }
-    public void food(JButton[][] grid, int width, int length){
+    protected void food(JButton[][] grid, int width, int length){
         int randomx = (int)(Math.random()*width);
         int randomy = (int)(Math.random()*length);
         grid[randomx][randomy].setEnabled(true);
         grid[randomx][randomy].setBackground(Color.ORANGE);
         grid[randomx][randomy].setBorderPainted(false);
         grid[randomx][randomy].setOpaque(true);
-
-
+    }
+    protected void keyPress(int keyID){
+        this.keyID = keyID;
+        System.out.println("key Pressed is: "+keyID);
     }
 
 }
