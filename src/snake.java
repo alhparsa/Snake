ea82d3c723bc2af7eebfb5b5262b50a289dd9fbc
hyoon1998 <hansooyoon@hotmail.com>
2017-06-05 15:09:44 +0800

    added few methods

--- src/snake.java
+++ src/snake.java
@@ -1,7 +1,16 @@
 /**
  * Created by Parsa on 2017-05-18.
  */
+
 public class snake {
-    int size=0;
+    int size = 2;
+
+    private int movement() {
+        return size;
+    }
+    private void food(int[][]dog,int width,int length){
+        int randomx = (int)Math.random()*width;
+        int randomy = (int)Math.random()*length;
+    }
 
 }
