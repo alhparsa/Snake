3a3138b657a84c0895eb710003ffcf50bbd201bc
Parsa <alh.parsa@gmail.com>
2017-06-06 11:27:06 +0800

    conditions have been added

--- src/snake.java
+++ src/snake.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by Parsa on 2017-05-18.
@@ -7,11 +8,38 @@ import java.awt.*;
 
 class snake {
     int size = 2;
-    int keyID;
+    int keyID; // stores the value of keyID
 
 
+    // variables to track the movement of the snake
+    boolean right = true;
+    boolean left = false;
+    boolean down = false;
+    boolean up = false;
 
-    private int movement() {
+
+    private int movement() { //method for movement
+        if (keyID == 38 && down == false){ //up arrow button is pressed
+            right = false;
+            up = true;
+            left = false;
+        }
+        if (keyID == 39 && left == false){ //right arrow button is pressed
+            right = true;
+            down = false;
+            up = false;
+        }
+        if (keyID == 40 && up == false){ //down arrow button is pressed
+            down = true;
+            left = false;
+            right = false;
+        }
+        if (keyID == 37 && right == false){ //left arrow button is pressed
+            left = true;
+            down = false;
+            up = false;
+        }
+
         return size;
     }
     protected void food(JButton[][] grid, int width, int length){
@@ -22,7 +50,7 @@ class snake {
         grid[randomx][randomy].setBorderPainted(false);
         grid[randomx][randomy].setOpaque(true);
     }
-    protected void keyPress(int keyID){
+    protected void keyPress(int keyID){ //receives and stores the value of keyID in this.keyID
         this.keyID = keyID;
         System.out.println("key Pressed is: "+keyID);
     }
